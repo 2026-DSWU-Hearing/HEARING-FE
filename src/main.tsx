5932fc481ab0d0c2b784b9dc60e1b8af039ea643
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-05-03 19:14:46 +0900

    setting: tanstack query 초기 설정 (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./shared/styles/global.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import './shared/styles/global.css';
+import App from './App.tsx';
+import { queryClient } from './shared/apis/queryClient.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+      <ReactQueryDevtools />
+    </StrictMode>
+  </QueryClientProvider>,
 );
