18de33f323dcd2ba87a5e68fda094f49b133532d
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-05-04 23:30:53 +0900

    setting: tailwind css 설치, global.css 설정 (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import './shared/styles/global.css';
 import App from '@/App';
-import { queryClient } from './shared/apis/queryClient.ts';
+import { queryClient } from '@/shared/apis/queryClient.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
