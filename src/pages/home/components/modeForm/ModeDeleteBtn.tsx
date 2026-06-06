390678c916709591f25739b437bebc1603a7d54c
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-06-07 06:12:25 +0900

    style: 모드 삭제하기 버튼 수정 (#31)

--- src/pages/home/components/modeForm/ModeDeleteBtn.tsx
+++ src/pages/home/components/modeForm/ModeDeleteBtn.tsx
@@ -12,7 +12,7 @@ const ModeDeleteBtn = ({
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="rounded-xl bg-neutral-400 px-8 py-3 text-lg font-bold text-neutral-900 disabled:bg-neutral-200 disabled:text-neutral-400"
+      className="w-[8.4rem] h-[2.0625rem] rounded-pill bg-[#5354504D]/30 px-xs py-xxs caption-xs-medium text-secondary border-[1px] border-neutral-700 disabled:bg-neutral-200 disabled:text-neutral-400"
     >
       모드 삭제하기
     </button>
