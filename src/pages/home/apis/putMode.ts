b52d3b2dcc71dd6ffcf8a847822af2da278ca4fc
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-06-09 00:10:07 +0900

    remove: mock 데이터 코드 삭제 (#51)

--- src/pages/home/apis/putMode.ts
+++ src/pages/home/apis/putMode.ts
@@ -3,26 +3,13 @@ import type {
   UpdateModeResponseTypes,
 } from '@/pages/home/types/modeTypes';
 import http from '@/shared/apis/axios';
-import { updateModeMock } from '@/pages/home/mocks/modeMock';
 import { normalizeModeIconKey } from '@/shared/components/icons/modes/modeIconMap';
 
-// 나중에 실제 서버랑 연동 시 false로 설정하면 됨 - 현재 mock 데이터 보여주는 용도
-const IS_MOCK = false;
 // 모드 수정
 export const putMode = async (
   modeId: number,
   modeData: UpdateModeRequestTypes,
 ): Promise<UpdateModeResponseTypes> => {
-  if (IS_MOCK) {
-    return {
-      ...updateModeMock,
-      mode_id: modeId,
-      name: modeData.name,
-      icon: modeData.icon,
-      sounds: modeData.sounds,
-    };
-  }
-
   const response = await http.put<UpdateModeResponseTypes>(
     `/api/v1/modes/${modeId}`,
     modeData,
