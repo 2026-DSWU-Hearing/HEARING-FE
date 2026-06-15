27a1dd49c239500b598075e2ee3a92db79a0443a
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-06-15 09:46:57 +0900

    comment: 필요 없는 주석 삭제 (#49)

--- src/pages/home/hooks/useDeleteModeSound.ts
+++ src/pages/home/hooks/useDeleteModeSound.ts
@@ -1,20 +1,3 @@
-// 실제 서버 연결 이후 바꿀 로직
-// import { useMutation, useQueryClient } from '@tanstack/react-query';
-// import { deleteModeSound } from '../apis/deleteModeSound';
-
-// export const useDeleteModeSound = () => {
-//   const queryClient = useQueryClient();
-
-//   return useMutation({
-//     mutationFn: ({ modeId, soundId }: { modeId: number; soundId: number }) =>
-//       deleteModeSound(modeId, soundId),
-//     onSuccess: (_, variables) => {
-//       queryClient.invalidateQueries({ queryKey: ['modes'] });
-//       queryClient.invalidateQueries({ queryKey: ['modes', variables.modeId] });
-//     },
-//   });
-// };
-
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteModeSound } from '../apis/deleteModeSound';
 import type { GetModeDetailResponseTypes } from '@/pages/home/types/modeTypes';
@@ -41,11 +24,6 @@ export const useDeleteModeSound = () => {
           };
         },
       );
-
-      // 실제 서버 연결 후에는 아래 방식으로 변경 가능
-      // queryClient.invalidateQueries({
-      //   queryKey: ['modes', variables.modeId],
-      // });
     },
   });
 };
--- src/pages/home/hooks/usePostMode.ts
+++ src/pages/home/hooks/usePostMode.ts
@@ -1,21 +1,3 @@
-// 실제 서버 연결 후 바꿀 로직
-// import { useMutation, useQueryClient } from '@tanstack/react-query';
-// import { postMode } from '@/pages/home/apis/postMode';
-// import type { CreateModeRequestTypes } from '@/pages/home/types/modeTypes';
-
-// export const usePostMode = () => {
-//   const queryClient = useQueryClient();
-
-//   return useMutation({
-//     mutationFn: (modeData: CreateModeRequestTypes) => postMode(modeData),
-//     onSuccess: () => {
-//       queryClient.invalidateQueries({ queryKey: ['modes'] });
-//     },
-//   });
-// };
-
-// src/pages/home/hooks/usePostMode.ts
-
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postMode } from '@/pages/home/apis/postMode';
 import type {
@@ -49,9 +31,6 @@ export const usePostMode = () => {
           modes: [...old.modes, newMode],
         };
       });
-
-      // 실제 서버 연결 후에는 아래 방식으로 변경 가능
-      // queryClient.invalidateQueries({ queryKey: ['modes'] });
     },
   });
 };
