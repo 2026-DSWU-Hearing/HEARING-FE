6bd3f9cb7e81ad428a783a5f858bf025313781de
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-06-21 17:10:37 +0900

    remove: 불필요한 코드 삭제 (#20)

--- src/pages/setting/components/ConnectDeviceBtn.tsx
+++ src/pages/setting/components/ConnectDeviceBtn.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ConnectDeviceBtn = () => {
   return <div></div>;
 };
--- src/pages/setting/components/HapticBlock.tsx
+++ src/pages/setting/components/HapticBlock.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const HapticBlock = () => {
   return <div></div>;
 };
