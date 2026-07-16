362f1b4430a6ae7b3637355f3c179ac88f5e533d
chubin <befanyelse@gmail.com>
2026-07-16 22:38:29 +0900

    fix: 로그인 접근 제한 코드 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -20,40 +20,46 @@ import NotificationSettingPage from '@/pages/setting/NotificationSettingPage';
 import ProfileEditPage from '@/pages/setting/ProfileEditPage';
 import Setting from '@/pages/setting/Setting';
 
+import ProtectedRoute from '@/routes/ProtectedRoute';
+
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
 
-      <Route path="/onboarding/nickname" element={<NicknamePage />} />
-      <Route path="/onboarding/disability" element={<DisabilityPage />} />
-      <Route path="/onboarding/terms" element={<TermsPage />} />
-      <Route
-        path="/onboarding/terms/:agreementId"
-        element={<TermsDetailPage />}
-      />
-      <Route path="/onboarding/hardware" element={<HwConnectPage />} />
-      <Route
-        path="/onboarding/hardware/connecting"
-        element={<HwConnectingPage />}
-      />
-      <Route
-        path="/onboarding/hardware/complete"
-        element={<HwCompletePage />}
-      />
+      {/* 로그인(액세스 토큰) 없이는 아래 라우트에 접근할 수 없다. */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
 
-      <Route path="/modes/new" element={<ModeCreatePage />} />
-      <Route path="/modes/:modeId/settings" element={<ModeEditPage />} />
-      <Route path="/notifications" element={<NotificationPage />} />
-      <Route path="/communication" element={<Communication />} />
-      <Route path="/live-sound" element={<LiveSound />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route
-        path="/setting/notification"
-        element={<NotificationSettingPage />}
-      />
-      <Route path="/setting/profile/edit" element={<ProfileEditPage />} />
+        <Route path="/onboarding/nickname" element={<NicknamePage />} />
+        <Route path="/onboarding/disability" element={<DisabilityPage />} />
+        <Route path="/onboarding/terms" element={<TermsPage />} />
+        <Route
+          path="/onboarding/terms/:agreementId"
+          element={<TermsDetailPage />}
+        />
+        <Route path="/onboarding/hardware" element={<HwConnectPage />} />
+        <Route
+          path="/onboarding/hardware/connecting"
+          element={<HwConnectingPage />}
+        />
+        <Route
+          path="/onboarding/hardware/complete"
+          element={<HwCompletePage />}
+        />
+
+        <Route path="/modes/new" element={<ModeCreatePage />} />
+        <Route path="/modes/:modeId/settings" element={<ModeEditPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/communication" element={<Communication />} />
+        <Route path="/live-sound" element={<LiveSound />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route
+          path="/setting/notification"
+          element={<NotificationSettingPage />}
+        />
+        <Route path="/setting/profile/edit" element={<ProfileEditPage />} />
+      </Route>
     </Routes>
   );
 };
