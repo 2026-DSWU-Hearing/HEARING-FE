b6e67a33142a14ccd4b2360501d5ab969fc31e9f
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-08-17 01:07:22 +0900

    fix: 주방 카테고리에서 주방도구 소리 삭제 (#119)

--- src/pages/home/utils/filterSoundCatalog.ts
+++ src/pages/home/utils/filterSoundCatalog.ts
@@ -4,9 +4,14 @@ import type {
 } from '@/pages/home/types/soundTypes';
 
 const REMOVED_CATEGORY_NAME = '음악';
+const REMOVED_SOUND_NAMES = new Set(['주방 도구']);
 
 export const filterVisibleCategories = (categories: CategoryTypes[]) =>
   categories.filter((category) => category.name !== REMOVED_CATEGORY_NAME);
 
 export const filterVisibleSounds = (sounds: SoundTypes[]) =>
-  sounds.filter((sound) => sound.category_name !== REMOVED_CATEGORY_NAME);
+  sounds.filter(
+    (sound) =>
+      sound.category_name !== REMOVED_CATEGORY_NAME &&
+      !REMOVED_SOUND_NAMES.has(sound.name),
+  );
